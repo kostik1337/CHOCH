7ba5debd28ee1a0dd2aced4499f6c8bc428279d4
Konstantin Guschin <gkostik1@gmail.com>
2020-08-28 03:44:24 +0700

    fix error

--- src/glsl/canvas_postproc_f_shader.glsl
+++ src/glsl/canvas_postproc_f_shader.glsl
@@ -4,6 +4,6 @@ uniform sampler2D tex;
 void main() {
     vec2 uv = gl_FragCoord.xy / res;
     uv.y = 1. - uv.y;
-    vec4 tex = texture2D(tex, uv);
-    gl_FragColor = vec4(mix(vec3(0.07, 0.07, 0.13), tex.rgb, tex.a), 1.);
+    vec4 texel = texture2D(tex, uv);
+    gl_FragColor = vec4(mix(vec3(0.07, 0.07, 0.13), texel.rgb, texel.a), 1.);
 }
