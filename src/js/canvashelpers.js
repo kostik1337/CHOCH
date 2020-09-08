10c2a3b90cef73733e8e599b36722332cd374319
Konstantin Guschin <gkostik1@gmail.com>
2020-09-08 15:49:24 +0700

    replace № with ! in typer, for some reason it's not working on itch

--- src/js/canvashelpers.js
+++ src/js/canvashelpers.js
@@ -13,7 +13,7 @@ async function print_2d(c, text, cancelFn, textUpdatedCb) {
 
     while (tail) {
         if (cancelFn()) return
-        if (tail.startsWith('№')) {
+        if (tail.startsWith('!')) {
             eval(tail.substring(1));
             tail = "";
         } else if (tail.startsWith('_')) {
@@ -38,52 +38,52 @@ let cliFont = "bold 22px 'Andale Mono', 'Courier New', monospace";
 let dialogFont = "bold italic 32px 'Lucida Sans Unicode', 'Lucida Grande', sans-serif";
 
 let startCutsceneData = (w, h) => [
-    `№+n;ms=50;font="${cliFont}";color='#0f0'`, "[totosz@vlt1337 ~]$ ",
+    `!+n;ms=50;font="${cliFont}";color='#0f0'`, "[totosz@vlt1337 ~]$ ",
     '_500',
-    "№;ms=50;color='#bbb';w=''", "hack https://asodih90xvy809.com/90as8y/",
+    "!;ms=50;color='#bbb';w=''", "hack https://asodih90xvy809.com/90as8y/",
 
-    "№+n;ms=300", ". . .  ",
+    "!+n;ms=300", ". . .  ",
 
-    "№n+n;color='#888';ms=50;w='+'",
-    "HTTP/2 404", "№+n",
-    "content-type: text/html", "№+n",
-    "content-length: 1565", "№+n",
-    `date: ${new Date().toDateString()}`, "№n+n",
+    "!n+n;color='#888';ms=50;w='+'",
+    "HTTP/2 404", "!+n",
+    "content-type: text/html", "!+n",
+    "content-length: 1565", "!+n",
+    `date: ${new Date().toDateString()}`, "!n+n",
 
-    "<!DOCTYPE html>", "№+n",
-    "<html lang=en>", "№+n",
-    "<meta charset=utf-8>", "№+n",
-    "<title>Error 404 (Not Found)!!1</title>", "№+n",
+    "<!DOCTYPE html>", "!+n",
+    "<html lang=en>", "!+n",
+    "<meta charset=utf-8>", "!+n",
+    "<title>Error 404 (Not Found)!!1</title>", "!+n",
     "<p>The requested URL was not found on this server. Tough luck :-)",
 
-    "№n+n,ms=500;color='#c00'", "[ERROR] Hacking failed with code 0x04729632",
+    "!n+n,ms=500;color='#c00'", "[ERROR] Hacking failed with code 0x04729632",
 
-    "№+n;color='#0f0'", "[totosz@vlt1337 ~]$ ",
+    "!+n;color='#0f0'", "[totosz@vlt1337 ~]$ ",
 
-    "№x_=x;y_=y", // save caret location
+    "!x_=x;y_=y", // save caret location
 
     '_1200',
-    `№;y=600;x=200;ms=60;w='';color='#f80';font="${dialogFont}"`,
-    "Damn it... They moved it again.", "№+n",
+    `!;y=600;x=200;ms=60;w='';color='#f80';font="${dialogFont}"`,
+    "Damn it... They moved it again.", "!+n",
     '_800',
-    `№;c.fillStyle='#000';c.shadowBlur=0;c.clearRect(0,y-60,${w},${h})`,
-    "№;ms=60;y=600;x=200", "You can hide it from me but I'll find it anyway!",
+    `!;c.fillStyle='#000';c.shadowBlur=0;c.clearRect(0,y-60,${w},${h})`,
+    "!;ms=60;y=600;x=200", "You can hide it from me but I'll find it anyway!",
     '_800',
-    `№;c.fillStyle='#000';c.shadowBlur=0;c.clearRect(0,y-60,${w},${h})`,
+    `!;c.fillStyle='#000';c.shadowBlur=0;c.clearRect(0,y-60,${w},${h})`,
 
-    `№;x=x_;y=y_;ms=50;color='#bbb';w='';font="${cliFont}"`, "hack https://asodih90xvy809.com/ --find-missing-page",
-    "№;ms=800", " ", "№;ms=50", "--please",
+    `!;x=x_;y=y_;ms=50;color='#bbb';w='';font="${cliFont}"`, "hack https://asodih90xvy809.com/ --find-missing-page",
+    "!;ms=800", " ", "!;ms=50", "--please",
 
     '_400',
-    `№;c.fillStyle='#000';c.shadowBlur=0;c.clearRect(0,0,${w},${h});y=40`,
-    "№n+n;ms=500;w='+',color='#fff'", "Initialize crawler ", "№;x=700;color='#0f0'", "[ OK ]",
-    "№+n;color='#fff'", "Generate search route ", "№;x=700;color='#0f0'", "[ OK ]",
-    "№+n;color='#fff'", "Calculate expression matcher ", "№;x=700;color='#0f0'", "[ OK ]",
-    "№+n;color='#fff'", "Perform automated search ", "_1200", "№;x=700;color='#f00'", "[FAIL]",
-    "№n+n;color='#fff';x=200", "Manual guidance required. Press enter to start",
-    "№w='';ms=200", ". . .  "
+    `!;c.fillStyle='#000';c.shadowBlur=0;c.clearRect(0,0,${w},${h});y=40`,
+    "!n+n;ms=500;w='+',color='#fff'", "Initialize crawler ", "!;x=700;color='#0f0'", "[ OK ]",
+    "!+n;color='#fff'", "Generate search route ", "!;x=700;color='#0f0'", "[ OK ]",
+    "!+n;color='#fff'", "Calculate expression matcher ", "!;x=700;color='#0f0'", "[ OK ]",
+    "!+n;color='#fff'", "Perform automated search ", "_1200", "!;x=700;color='#f00'", "[FAIL]",
+    "!n+n;color='#fff';x=200", "Manual guidance required. Press enter to start",
+    "!w='';ms=200", ". . .  "
 ];
 
 let endCutsceneData = (w, h) => [
-    `№+n;ms=100;font="${cliFont}";color='#0f0';w=''`, "Congratulations! You won!"
+    `!+n;ms=100;font="${cliFont}";color='#0f0';w=''`, "Congratulations! You won!"
 ];
