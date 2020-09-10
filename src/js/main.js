b4790fc0ceca42fbfd75c8267a05409b87496128
Qwe Qwe <qwe@qwe.qwe>
2020-09-10 19:37:40 +0700

    resize canvas

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ function main() {
     var canvas = document.querySelector('#glcanvas');
     var gl = canvas.getContext('webgl');
 
+    handleResize();
+
     // @ifdef DEBUG
     if (!gl) {
         console.log('Unable to initialize WebGL. Your browser or machine may not support it.');
@@ -34,10 +36,19 @@ function loop() {
     window.requestAnimationFrame(loop);
 }
 
+function handleResize() {
+    ["glcanvas", "canvas2d"].forEach(id =>{
+        const canvas = document.getElementById(id);
+        canvas.width = window.innerWidth, 
+        canvas.height = window.innerHeight;
+    });
+}
+
 var keyFunction = (e, pressed) => {
     if (!e.repeat) onKeyEvent(e.which, pressed)
 };
 window.addEventListener('keydown', e => keyFunction(e, true));
 window.addEventListener('keyup', e => keyFunction(e, false));
 window.addEventListener('load', main, false);
+window.addEventListener('resize', handleResize);
 window.setInterval(() => update(), 16);
