24cb333f83120d081476164edb4f9844dedf1498
Konstantin Guschin <gkostik1@gmail.com>
2020-09-11 00:32:56 +0700

    screen borders blurred dark

--- src/glsl/postproc_f_shader.glsl
+++ src/glsl/postproc_f_shader.glsl
@@ -1,19 +1,17 @@
 uniform vec2 res;
 uniform sampler2D tex;
 uniform vec3 fact; // fact.x - death factor, fact.y - checkpoint factor, fact.z - end factor
-// // death factor
-// uniform float df;
-// // checkpoint factor
-// uniform float cf;
 
 void main() {
     vec2 uv = gl_FragCoord.xy / res;
-    //uv.y = 1. - uv.y;
+    vec2 uvn = abs(uv*2. - 1.);
     float aberrationSize = 0.003 + 0.015*fact.x;
     vec3 texel = vec3(
         texture2D(tex, uv-vec2(aberrationSize, 0.)).r,
         texture2D(tex, uv).g,
         texture2D(tex, uv+vec2(aberrationSize, 0.)).b
     );
-    gl_FragColor = vec4(texel + vec3(0.,fact.y,0.) + fact.z, 1.);
+    float darkLen = 0.6;
+    float dark = max(smoothstep(1.-darkLen, 1., uvn.x), smoothstep(1.-darkLen, 1., uvn.y));
+    gl_FragColor = vec4(mix(texel, vec3(0.), 0.25*dark) + vec3(0.,fact.y,0.) + fact.z, 1.);
 }
